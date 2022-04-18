1c29bbd4e7a764a6d932cd4a402f4466e66d778a
FlutLab.io <>
2022-04-18 04:13:30 +0000

    Autosave

--- lib/providers/stockopname_detail_provider.dart
+++ lib/providers/stockopname_detail_provider.dart
@@ -1,6 +1,6 @@
 import 'package:stockopname_leimena/models/item.dart';
 import 'package:stockopname_leimena/services/so_detail_service.dart';
-import 'package:stockopname_leimena/views/views.dart';
+// import 'package:stockopname_leimena/views/views.dart';
 
 import '../models/stockopname_detail.dart';
 
@@ -10,20 +10,13 @@ class StockopnameDetailProvider {
     return soDetailService.getSoDetail(soId);
   }
 
-  static Future<bool> addSoItem(Item item, String soId ) async
-  {
+  static Future<bool> addSoItem(Item item, String soId) async {
     final soDetailService = SoDetailService();
     return soDetailService.storeProduct(item, soId);
   }
 
   static searchStockopnameDetail(String query, String soId) async {
-    var result = soDetail
-        .where((item) =>
-            (item.sku!.toLowerCase().contains(query.toLowerCase()) ||
-            item.barcode!.toLowerCase().contains(query.toLowerCase()) ||
-            item.item!.toLowerCase().contains(query.toLowerCase())) &&
-            item.soId!.toLowerCase().contains(soId.toLowerCase()))
-        .toList();
-    return result;
+    final soDetailService = SoDetailService();
+    return soDetailService.getSoDetail(soId, query: query);
   }
 }
--- lib/services/so_detail_service.dart
+++ lib/services/so_detail_service.dart
@@ -4,24 +4,30 @@ import 'package:stockopname_leimena/models/stockopname_detail.dart';
 
 import 'database_service.dart';
 
-class SoDetailService
-{
+class SoDetailService {
   late DatabaseService database;
 
-  SoDetailService(){
+  SoDetailService() {
     database = DatabaseService();
   }
 
   Future<bool> storeProduct(Item item, String soId) async {
     Database db = await database.database;
     const sql = 'INSERT INTO so_detail(id, so_id, product_id, so_stock) VALUES(?,?,?,?)';
-    final result = await db.rawInsert(sql, [null, soId, item.id, 0]);
+    final result = await db.rawInsert(sql, [
+      null,
+      soId,
+      item.id,
+      0
+    ]);
     return result > 0;
   }
 
-  Future<List<StockopnameDetail>> getSoDetail(String soId) async {
+  Future<List<StockopnameDetail>> getSoDetail(String soId, {String? query}) async {
     Database db = await database.database;
-    const sql = '''
+    List<Map<String, Object?>> mapObject;
+    if (query == null) {
+      const sql = '''
     SELECT 
     so_detail.id, 
     so_detail.so_id, 
@@ -36,7 +42,30 @@ class SoDetailService
     JOIN stockopname ON so_detail.so_id = stockopname.so_id
     WHERE so_detail.so_id = ?
     ''';
-    final mapObject = await db.rawQuery(sql, [soId]); 
+      mapObject = await db.rawQuery(sql, [
+        soId
+      ]);
+    } else {
+      final sql = '''
+    SELECT 
+    so_detail.id, 
+    so_detail.so_id, 
+    product.sku, 
+    product.product_name, 
+    product.barcode, 
+    product.uom, 
+    so_detail.so_stock, 
+    product.last_stock
+    FROM so_detail
+    JOIN product ON so_detail.product_id = product.id
+    JOIN stockopname ON so_detail.so_id = stockopname.so_id
+    WHERE so_detail.so_id = ? AND 
+    (product.sku LIKE '%$query%' OR product.product_name LIKE '%$query%' OR product.barcode LIKE '%$query%')
+    ''';
+      mapObject = await db.rawQuery(sql, [
+        soId
+      ]);
+    }
     List<StockopnameDetail> result = [];
     for (var i = 0; i < mapObject.length; i++) {
       result.add(StockopnameDetail.fromMapObject(mapObject[i]));
