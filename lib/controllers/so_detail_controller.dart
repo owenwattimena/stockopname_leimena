40e13def0afd8222ebddce5c94c985a99d1c8c14
FlutLab.io <>
2022-04-18 07:18:32 +0000

    Autosave

--- lib/controllers/so_detail_controller.dart
+++ lib/controllers/so_detail_controller.dart
@@ -50,9 +50,10 @@ class SoDetailController extends GetxController {
     // );
     List<StockopnameDetail> soDetailItem = listSoDetail.value.where((element) => element.sku == item.sku).toList();
     if (soDetailItem.isEmpty) {
-      listSoDetail.update((val) async {
-        StockopnameDetail soDetail = await StockopnameDetailProvider.addSoItem(item, soId.value);
-        val!.insert(0, soDetail);
+      StockopnameDetailProvider.addSoItem(item, soId.value).then((value) {
+        listSoDetail.update((val) {
+          val!.insert(0, value);
+        });
       });
 
       return true;
