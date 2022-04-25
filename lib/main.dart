f2a4cafcc88caf9d3e1ca592f413c0ff215c8b0b
FlutLab.io <>
2022-04-25 04:43:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'views/views.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stockopname',
+      title: 'Stock Counter',
       theme: ThemeData(
         // This is the theme of your application.
         //
